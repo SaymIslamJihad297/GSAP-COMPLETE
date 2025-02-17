244e07ffb147e3829427aa6639ce76fe8f8b2e92
Saym Islam Jihad <kojn6193@gmail.com>
2025-02-17 19:39:31 +0600

    basic gsap

--- basics/script.js
+++ basics/script.js
@@ -1,20 +1,41 @@
-gsap.to("#box1", {
+// gsap.to("#box1", {
+//     x: 1000,
+//     duration: 2,
+//     delay: 1,
+//     borderRadius: "50%",
+//     repeat: -1,
+//     yoyo: true,
+// })
+// gsap.from("#box2", {
+//     x: 500,
+//     y: 500,
+//     delay: 1,
+//     duration: 2,
+//     rotate: 360,
+// })
+// gsap.from(".showH1", {
+//     opacity: 0,
+//     duration: 2,
+//     delay: 1,
+//     y: 30,
+//     stagger: 0.3,
+// })
+
+var tl = gsap.timeline();
+
+tl.to("#box1", {
     x: 1000,
     duration: 2,
     delay: 1,
     borderRadius: "50%",
 })
-gsap.from("#box2", {
-    x: 500,
-    y: 500,
-    delay: 1,
+tl.to("#box2", {
+    x: 1000,
     duration: 2,
-    rotate: 360,
+    borderRadius: "50%",
 })
-gsap.from(".showH1", {
-    opacity: 0,
+tl.to("#box3", {
+    x: 1000,
     duration: 2,
-    delay: 1,
-    y: 30,
-    stagger: 0.3,
+    borderRadius: "50%",
 })
